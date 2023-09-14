ca443c8f5fe73e988fa8384c3ecfe54d177c3662
abdurrozaqf <rozaqa27@gmail.com>
2023-09-14 18:55:05 +0700

    Disable onboarding from aplication

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/onboarding',
+      initialRoute: '/homeview',
       getPages: AppPage.pages,
     );
   }
